7d929cf8334115af126226f8c69bb3923733f3dd
sameekshasingh1809 <sameeksha10a37339@gmail.com>
2025-11-11 14:02:55 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Header.css';
 
 export const Header = () => {
   return (
     <div className="header-container">
       <div className="header-card">
         <img 
-          src="/myphoto.jpg" 
+          src={`${process.env.PUBLIC_URL}/myphoto.jpg`} 
           alt="Sameeksha Singh" 
           className="header-photo"
         />
-        <h2>Sameeksha’s Expense Tracker </h2>
+        <h2>Sameeksha’s Expense Tracker</h2>
         <p className="header-subtitle">Track your spending. Balance your vibe ✨</p>
       </div>
     </div>
